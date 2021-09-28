b18649efe709aea65b2391fcad336d139f340738
giuseppescalesse <scalesse82@gmail.com>
2021-09-28 11:27:14 +0200

    add map

--- lib/Screen/ListaAziende.dart
+++ lib/Screen/ListaAziende.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:alphabet_list_scroll_view/alphabet_list_scroll_view.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:report_visita_danilo/Models/Azienda.dart';
+import 'package:report_visita_danilo/Utils/ZoomButtonsPluginOption.dart';
 
 class ListaAziende extends StatefulWidget {
   @override
@@ -9,6 +14,11 @@ class ListaAziende extends StatefulWidget {
 }
 
 class ListaAziendeState extends State<ListaAziende> {
+
+  late final MapController _mapController;
+  List<Marker> _markers = [];
+  Position? _currentLocation;
+
 
   bool viewMap=false;
   final TextEditingController _controller = new TextEditingController();
@@ -19,6 +29,83 @@ class ListaAziendeState extends State<ListaAziende> {
     Azienda(nome: "bbbbb"),
     Azienda(nome: "ccccc")
   ];
+
+
+
+  @override
+  void initState() {
+    super.initState();
+    _mapController = MapController();
+    _getCurrentLocation();
+  }
+
+
+  void _getCurrentLocation() {
+    Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.best,
+        forceAndroidLocationManager: true)
+        .then((Position position) {
+      setState(() {
+        _currentLocation = position;
+       // _buildBody();
+      });
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
+
+  void _prepareMarker() {
+
+    if(searchresult.isEmpty){
+
+    _markers = listaAziende.map((element) {
+
+      Marker marker = Marker(
+          point:LatLng(element.lat!, element.lng!),
+          width: ScreenUtil().setWidth(49),
+          height: ScreenUtil().setHeight(65),
+          builder: (context) => Opacity(
+            child: Container(
+                alignment: Alignment.bottomCenter,
+                child: Image.asset(
+                  "assets/marker.png",
+                  width: ScreenUtil().setWidth(49),
+                  height: ScreenUtil().setHeight(65),
+                )),
+            opacity: 1.0,
+          ));
+      return marker;
+    }).toList();
+    } else{
+      void _prepareMarker() {
+        _markers = searchresult.map((element) {
+          Marker marker = Marker(
+              point: LatLng(element.lat!, element.lng!),
+              width: ScreenUtil().setWidth(49),
+              height: ScreenUtil().setHeight(65),
+              builder: (context) => Opacity(
+                child: Container(
+                    alignment: Alignment.bottomCenter,
+                    child: Image.asset(
+                      "assets/marker.png",
+                      width: ScreenUtil().setWidth(49),
+                      height: ScreenUtil().setHeight(65),
+                    )),
+                opacity: 1.0,
+              ));
+          return marker;
+        }).toList();
+      }
+
+    }
+  }
+
+
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -172,6 +259,92 @@ class ListaAziendeState extends State<ListaAziende> {
   }
 
   buildBodyMap() {
-    return Container();
+    return SafeArea(
+      top:false,
+      bottom: false,
+      child: Padding(
+          padding: EdgeInsets.all(0.0),
+          child: Column(children: [Flexible(child: _buildFlatterMap(context))]))
+
+    );
   }
+
+
+
+  Widget _buildFlatterMap(BuildContext context) {
+    late LatLng _location;
+    if (_currentLocation != null) {
+      _location =
+          LatLng(_currentLocation!.latitude, _currentLocation!.longitude);
+    } else {
+
+    }
+
+    FlutterMap _flutterMap = FlutterMap(
+        mapController: _mapController,
+        options: MapOptions(
+          center: _location,
+          zoom: 8.0,
+          interactiveFlags: InteractiveFlag.all - InteractiveFlag.rotate,
+          plugins: [ZoomButtonsPlugin(), MarkerClusterPlugin()],
+          onTap: (tapPosition, point) => null,
+        ),
+        layers: [
+          //LocationMarkerLayerOptions(),
+          TileLayerOptions(
+            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            subdomains: ['a', 'b', 'c'],
+          ),
+
+
+          MarkerClusterLayerOptions(
+            maxClusterRadius: 50,
+            disableClusteringAtZoom: 8,
+            size: Size(ScreenUtil().setWidth(50), ScreenUtil().setWidth(50)),
+            fitBoundsOptions: FitBoundsOptions(
+              padding: EdgeInsets.all(ScreenUtil().setWidth(50)),
+            ),
+            markers: _markers,
+            polygonOptions: PolygonOptions(
+                borderColor: Colors.blueAccent,
+                color: Colors.black12,
+                borderStrokeWidth: 2),
+            onMarkerTap: (marker) {
+              //_showSheet(marker.key.toString());
+              //far visualizzare un toast
+            },
+
+            builder: (context, markers) {
+              return Container(
+                alignment: Alignment.center,
+                decoration:
+                BoxDecoration(color: Colors.orange, shape: BoxShape.circle),
+                child: Text('${markers.length}'),
+              );
+            },
+          )
+        ],
+        nonRotatedLayers: [
+          ZoomButtonsPluginOption(
+            minZoom: 5,
+            maxZoom: 19,
+            mini: true,
+            padding: 8,
+            zoomOutColor: Colors.red,
+            zoomInColor:  Colors.red,
+            zoomInColorIcon: Colors.white,
+            zoomOutColorIcon: Colors.white,
+            alignment: Alignment.bottomLeft,
+          )
+
+        ]);
+    return _flutterMap;
+  }
+
+
+
+
+
+
+
 }
