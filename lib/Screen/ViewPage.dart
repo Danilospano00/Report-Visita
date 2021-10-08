f7f34ee1766b71b0c4a35b600d497ba8468dfc91
Danilospano00 <danilospano00@gmail.com>
2021-10-08 15:50:29 +0200

    Terminata pagina ScegliAllerta.dart con salvataggio delle soglie nelle shared preferences

--- lib/Screen/ViewPage.dart
+++ lib/Screen/ViewPage.dart
@@ -51,7 +51,7 @@ class ViewPageState extends State<ViewPage> {
                 color: Colors.white,
                 onPressed: () {
                   Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => ScegliAllerta()));
+                      MaterialPageRoute(builder: (context) => AccountEmpty()));
                 },
               ),
               Spacer(),
